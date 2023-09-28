fb853cf0e63cc2e5e865594ce461fa2ec4c01acd
ssvitlana31 <130747746+ssvitlana31@users.noreply.github.com>
2023-09-28 13:20:35 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let interval;
+
+startBtnEl.addEventListener('click', onStart);
+stopBtnEl.addEventListener('click', onStop);
+
+function onStart(e) {
+  interval = setInterval(callback, 1000);
+  function callback() {
+    e.target.disabled = true;
+    stopBtnEl.disabled = false;
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }
+}
+
+function onStop(e) {
+  e.target.disabled = true;
+  startBtnEl.disabled = false;
+  clearInterval(interval);
+}
